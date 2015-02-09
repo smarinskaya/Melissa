2bb1c493353b413d5336424b6473b1429778a94e
Svetlana Marinskaya <svetlana.marinskaya@gmail.com>
2015-02-09 11:34:19 -0500

    I was setting mode before the library loaded. Trying to fix that.

--- lib/melissa/config.rb
+++ lib/melissa/config.rb
@@ -10,14 +10,6 @@ module Melissa
     attr_accessor :geo_point_lib, :geo_point_lib_loaded
 
     def initialize
-
-      puts "In the beginning of Config#initialize:  @addr_obj_library_loaded: #{@addr_obj_library_loaded.inspect}"
-      if defined?(@addr_obj_library_loaded) && @addr_obj_library_loaded
-        @mode = :live
-      else
-        @mode = :mock
-      end
-
       puts "In Config.new: #{@mode}"
 
       #It is recommended to read the following config options from environment variables
@@ -26,12 +18,12 @@ module Melissa
       #MD_LICENSE. This allows you to update your license string without editing
       #and recompiling your code
 
-      self.config_path   = ENV['MELISSA_CONFIG_PATH']   if ENV['MELISSA_CONFIG_PATH']
-      self.home          = ENV['MELISSA_HOME']          if ENV['MELISSA_HOME']
-      @data_path         = ENV['MELISSA_DATA_PATH']     if ENV['MELISSA_DATA_PATH']
-      @addr_obj_lib      = ENV['MELISSA_ADDR_OBJ_LIB']  if ENV['MELISSA_ADDR_OBJ_LIB']
-      @geo_point_lib     = ENV['MELISSA_GEO_POINT_LIB'] if ENV['MELISSA_GEO_POINT_LIB']
-      @license           = ENV['MD_LICENSE']            if ENV['MD_LICENSE']
+      self.config_path = ENV['MELISSA_CONFIG_PATH'] if ENV['MELISSA_CONFIG_PATH']
+      self.home = ENV['MELISSA_HOME'] if ENV['MELISSA_HOME']
+      @data_path = ENV['MELISSA_DATA_PATH'] if ENV['MELISSA_DATA_PATH']
+      @addr_obj_lib = ENV['MELISSA_ADDR_OBJ_LIB'] if ENV['MELISSA_ADDR_OBJ_LIB']
+      @geo_point_lib = ENV['MELISSA_GEO_POINT_LIB'] if ENV['MELISSA_GEO_POINT_LIB']
+      @license = ENV['MD_LICENSE'] if ENV['MD_LICENSE']
     end
 
     #you can configure yml_path from your code using:
@@ -44,7 +36,7 @@ module Melissa
           line.strip!
           next if line.empty? || line[0] == '#'
           equal_index = line.index('=')
-          key   = line[0,equal_index].strip.downcase
+          key = line[0, equal_index].strip.downcase
           value = line[(equal_index+1)..-1].strip
           send("#{key}=", value)
           puts "In config_path=, @license=#{license}"
@@ -56,9 +48,20 @@ module Melissa
 
     def home=(home)
       @addr_obj_lib = "#{home}/AddrObj/libmdAddr.so"
-      @geo_obj_lib  = "#{home}/GeoObj/libmdGeo.so"
-      @data_path    = "#{home}/data"
+      @geo_obj_lib = "#{home}/GeoObj/libmdGeo.so"
+      @data_path = "#{home}/data"
       puts "In home=, @addr_obj_lib=#{@addr_obj_lib}"
+    end
+
+    def addr_obj_lib_loaded=(value)
+      @addr_obj_lib_loaded = value
+      @mode ||= :live if value
+      puts "Should set mode to live #{mode}"
+    end
+
+    def geo_point_library_loaded=(value)
+      @geo_point_lib_loaded = value
     end
   end
 end
+
